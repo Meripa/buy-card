1920abd0013afd70215736e7bf6a77cd3176680c
Meris Olen <19meris@gmail.com>
2024-06-21 19:50:46 +0300

    korras

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
-let result = document.getElementById("GFG");
- 
-function changeColor(color) {
-    document.body.style.background = color;
-}
- 
-function myFunc() {
-    changeColor('yellow');
-    result.innerHTML = "Background Color changed";
-}        
+const toggleBtn = document.getElementById('check');
+const price = document.querySelectorAll('.price');
+const duration = document.querySelectorAll('.duration');
+
+
+window.addEventListener("DOMContentLoaded", () => {
+    for(let i=0; i<duration.length; i++){
+        duration[i].innerText = "/month";
+        toggleBtn.addEventListener("click", () => {
+            if(toggleBtn.checked == true){
+                duration[i].innerText = "";
+                duration[i].innerText = "/year";
+            }else{
+                duration[i].innerText = "";
+                duration[i].innerText = "/month";
+            }
+        })
+    }
+})
+
+toggleBtn.addEventListener("click", () => {
+    for(let j=0; j<price.length; j++){
+        if(toggleBtn.checked == true){
+            price[j].innerText *= 11;
+        }
+        else{
+            price[j].innerText /= 11;
+        }
+    }
+})
